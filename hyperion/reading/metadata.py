67e96f5c497bc18046dbae5a686cad11d1bd90a9
Stemonitis <atokhtamysh@gmail.com>
2020-06-01 02:21:51 -0400

    Metadata plotting functions

--- hyperion/reading/metadata.py
+++ hyperion/reading/metadata.py
@@ -5,7 +5,8 @@ Created on Sat May 30 18:16:18 2020
 @author: mysh
 """
 #%%Reading libraries and modules
-
+from tvtk.api import tvtk
+from mayavi import mlab
 import matplotlib.pyplot as plt
 import numpy as np
 from pyhdf.SD import SD,SDC #utility to read hdf files, tutorial: https://www.hdfeos.org/software/pyhdf.php
@@ -25,6 +26,30 @@ def show_metadata_datasets(meta):
     #'Gain Coefficients': (('fakeDim4', 'fakeDim5'), (242, 256), 5, 2), 
     #'Spectral Center Wavelengths': (('fakeDim0', 'fakeDim1'), (242, 256), 5, 0)}
     return meta.datasets()
+'''Reading spectral center wavelength datafile'''
+
+def read_spectral_center(meta):
+    DATAFIELD_NAME='Spectral Center Wavelengths'
+    spbandshdf=meta.select(DATAFIELD_NAME)
+    #spbandshdf.dimensions()
+    '''Save bandwidth as a numpy array'''
+    return np.array(spbandshdf[:,:])
+    #spbands.shape
+    #(242, 256)
+    #matshow(spbands.T, cmap='spectral')
+    #plt.colorbar()   
+    
+def read_and_plot_spectral_center(meta):
+    DATAFIELD_NAME='Spectral Center Wavelengths'
+    spcenterhdf=meta.select(DATAFIELD_NAME)
+    spcenter=np.array(spcenterhdf[:,:])
+    #spbandshdf.dimensions()
+    plt.matshow(spcenter.T, cmap='spectral')
+    plt.colorbar()
+    '''Save bandwidth as a numpy array'''
+    return spcenter
+    #spbands.shape
+    #(242, 256)
     
 '''Reading spectral bandwidth datafile'''
 def read_spectral_bandwidth(meta):
@@ -37,6 +62,16 @@ def read_spectral_bandwidth(meta):
     #(242, 256)
     #matshow(spbands.T, cmap='spectral')
     #plt.colorbar()   
+def read_and_plot_spectral_bandwidth(meta):
+    DATAFIELD_NAME='Spectral Bandwidths'
+    spbandshdf=meta.select(DATAFIELD_NAME)
+    spbands=np.array(spbandshdf[:,:])
+    #spbandshdf.dimensions()
+    plt.matshow(spbands.T, cmap='spectral')
+    plt.colorbar()
+    return spbands
+    #spbands.shape
+    #(242, 256)
 
 '''Reading mask datafile'''
 def read_mask(meta):
@@ -45,7 +80,19 @@ def read_mask(meta):
     #maskhdf.dimensions()
     #functions available :dir()
     return np.array(maskhdf[:,:,:])
-
+def read_and_plot_mask(meta):
+    DATAFIELD_NAME1='Flag Mask'
+    maskhdf=meta.select(DATAFIELD_NAME1)
+    mask=np.array(maskhdf[:,:,:])
+    data = mask
+    i = tvtk.ImageData(spacing=(2, 2, 2), origin=(0, 0, 0))
+    i.point_data.scalars = data.ravel()
+    i.point_data.scalars.name = 'scalars'
+    i.dimensions = data.shape
+    mlab.pipeline.surface(i)
+    mlab.colorbar(orientation='vertical')
+    mlab.show()
+    return mask
 '''Reading gain data file'''
 
 'Gain Coefficients'
@@ -53,9 +100,19 @@ def read_gain(meta):
     DATAFIELD_NAME2='Gain Coefficients'
     gainhdf=meta.select(DATAFIELD_NAME2)
     gainhdf.dimensions()
-    return np.array(gainhdf[:,:,:])
+    return np.array(gainhdf[:,:])
+    
+def read_and_plot_gain(meta):
+    DATAFIELD_NAME2='Gain Coefficients'
+    gainhdf=meta.select(DATAFIELD_NAME2)
+    gain=np.array(gainhdf[:,:])
+    plt.matshow(gain.T, cmap='spectral')
+    plt.colorbar()
+    return gain
 
 '''Due to low signal for some channels, and to reduce the VNIR-SWIR
 overlap region, some of these spectral channels are not calibrated. The uncalibrated channels are
 set to zero. The channels are not removed from the file so the final data set is the same size as the
 initial data set. '''
+#%%
+
